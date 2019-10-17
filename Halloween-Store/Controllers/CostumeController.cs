fc7abb7c568e7e12cca00fc675c6bed09b2620dc
Tanner Disney <disneytanner@gmail.com>
2019-10-17 11:04:27 -0700

    Added Costume Controller and Started CRUD For Costumes

---
+++ Halloween-Store/Controllers/CostumeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Halloween_Store.Controllers
+{
+    public class CostumeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
