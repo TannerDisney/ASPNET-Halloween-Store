dae29c955c8202d6e88f0e655212a553ea420c87
Tanner Disney <disneytanner@gmail.com>
2019-10-17 12:09:31 -0700

    Added Add and Update View, Updated Layout to Default to Index

--- Halloween-Store/Controllers/CostumeController.cs
+++ Halloween-Store/Controllers/CostumeController.cs
@@ -2,21 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Halloween_Store.Data;
+using Halloween_Store.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Halloween_Store.Controllers
 {
     public class CostumeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext context;
+
+        public CostumeController(ApplicationDbContext dbContext)
         {
-            return View();
+            context = dbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Costume> c = await CostumeDb.GetAllCostumes(context);
+            return View(c);
         }
 
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Costume costume)
+        {
+            if (ModelState.IsValid)
+            {
+                await CostumeDb.AddCostume(costume, context);
+                return RedirectToAction("Index");
+            }
+
+            return View(costume);
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            Costume costume = await CostumeDb.GetCostumeById(id, context);
+            return View(costume);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Costume costume)
+        {
+            if (ModelState.IsValid)
+            {
+                await CostumeDb.UpdateCostume(costume, context);
+                return RedirectToAction("Index");
+            }
+
+            return View(costume);
         }
     }
 }
